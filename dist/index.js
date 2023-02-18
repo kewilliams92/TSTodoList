8c478eea9e95b18de607669ecfea84257c9a012b
Kevin <kewilliams2017@hotmail.com>
2023-02-18 14:08:47 -0500

    adding in interface for todo

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,33 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg);
-    console.log(msg);
+const todos = [];
+//NON-null assertion operator "!"
+const btn = document.getElementById("btn");
+const input = document.getElementById('todoInput');
+const form = document.querySelector("form");
+const list = document.querySelector("#todoList");
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = '';
 }
-printDouble("hello world :)))))");
+function createTodo(todo) {
+    const newLI = document.createElement('li');
+    const checkBox = document.createElement('input');
+    checkBox.type = "checkbox";
+    newLI.append(todo.text);
+    newLI.append(checkBox);
+    list?.append(newLI);
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", function() {
+//      alert(input.value);
+//      input.value = '';
+// });
+// let mystery: unknown = "hello World!"
+// type assertion "as"
+// const numChars = (mystery as string).length
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,47 @@
-function printDouble(msg: string){
-    console.log(msg)
-    console.log(msg)
+interface Todo {
+    text: string,
+    completed: boolean
 }
 
-printDouble("hello world :)))))")
+const todos: Todo[] = []
+
+//NON-null assertion operator "!"
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById('todoInput')! as HTMLInputElement;
+const form = document.querySelector("form")!;
+const list = document.querySelector("#todoList")!;
+
+function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    }
+    createTodo(newTodo)
+    todos.push(newTodo)
+    input.value = ''
+}
+
+function createTodo(todo: Todo){
+    const newLI = document.createElement('li')
+    const checkBox = document.createElement('input');
+    checkBox.type = "checkbox"
+    newLI.append(todo.text)
+    newLI.append(checkBox)
+    list?.append(newLI)
+
+}
+
+form.addEventListener("submit", handleSubmit)
+
+// btn.addEventListener("click", function() {
+//      alert(input.value);
+//      input.value = '';
+// });
+
+
+
+// let mystery: unknown = "hello World!"
+// type assertion "as"
+// const numChars = (mystery as string).length
+
